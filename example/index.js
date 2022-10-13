28363edba2eacda2b9b224f57f2cbf564a3678c5
Brad Garropy <bradgarropy@gmail.com>
2022-10-13 14:00:22 -0500

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,4 @@
+import {sayHello} from "../dist/index.js"
+
+console.log(sayHello)
+sayHello("Brad")
---
+++ tests/index.test.ts
@@ -0,0 +1,11 @@
+import {expect, test} from "vitest"
+
+import {sayHello} from "../src"
+
+test("says hello world", () => {
+    expect(sayHello()).toEqual("Hello, world!")
+})
+
+test("says hello brad", () => {
+    expect(sayHello("Brad")).toEqual("Hello, Brad!")
+})
